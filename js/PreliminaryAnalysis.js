25e6c242ccb05017b4691c9e029633b15a1cfe39
Ruan Pingcheng <ruanpingcheng@gmail.com>
2015-12-17 11:37:03 +0800

    Finish js file without backend communication

--- js/PreliminaryAnalysis.js
+++ js/PreliminaryAnalysis.js
@@ -1,17 +1,17 @@
 
-$("#progress").hide();
-$("#processing").hide();
+$("#waiting").hide();
+$("#senti").hide();
+$("#emotion").hide();
     
 $(":submit").click(function(){
-	
-	
-	
+	$("#waiting").show();
+	$("#senti").hide();
+	$("#emotion").hide();
+		
 	var sentence = $("#sentence").val();
 	var selectedMode = $('#mode input:radio:checked').val();
 	var animateBar=setInterval(animateProgressBar,1000)
 	var result;
-	$("#progress").show();
-	$("#processing").show();
 	
 
 	if(selectedMode == "coarse"){
@@ -51,46 +51,91 @@ function animateProgressBar(){
 	
 }
 
+function sentiCallback(data){
+	$("#waiting").hide();
+	$("#emotion").hide();
+	
+	var sentiment = data.sentiment;//to be changed later
+	var imgSrc = "images/sentiment/" + sentiment + ".jpg";
+	
+	$("#senti img").attr("src",imgSrc);
+	$("#senti h1").text(cap(sentiment));
+	$("#senti").show();
+	
+}
 
+function emotionCallback(data){
+	$("#waiting").hide();
+	$("#senti").hide();
+	
+	sentiment = data.sentiment;
+	emotions = data.emotions;
+	
+	var imgSrc = "images/sentiment/" + sentiment + ".jpg";
+	
+	$("#emotion img").attr("src",imgSrc);
+	$("#emotion h1").text(cap(sentiment));	
+	
+	$("#emotion div div").remove();
+	
+	emotions.forEach(function(value, index, ar){
+		
+		var item = '<div class="col-xs-3">';
+
+
+		item += "<img src='images/emotion/" + value   +  ".jpeg'  width='100' height='100'/>";
+		item += "<h3 align='center' style='font-style:italic'>";
+		item += cap(value) + "</h3>";
+		item += "</div>"
+	
+		$("#emotion div.row").append(item);
+		
+	})
+	
+	
+	
+	$("#emotion").show();
+	
+}
 
 function coarseChinese(s){  
-	var result = 
-	$.getJSON("test/positive.json",null,function(data){
-		console.log(data);
-		$("#p2").html(cap(data.sentiment));
-	});	
-	console.log("I am here");
+	//Change later
+	$.getJSON("test/positive.json",s,sentiCallback);
 }
 
 function fineChinese(s){ 
 	
-	$.ajax({
-	//url: "http://localhost/~ruanpingcheng/product3.json",
-	url: "test/positive.json",
 	
-	type: "GET",
-	dataType:"json",
-		error:function(XMLHttpRequest, textStatus, errorThrown) {    
-		              alert(XMLHttpRequest.status);   // 200    
-		alert(errorThrown);  // SyntaxError: Unexpected end of input    
-		          }    ,
-	success:function(data){
-			console.log(data)
-	}
-	});
+	$.getJSON("test/emotion.json",s,emotionCallback);
 	
+	// $.ajax({
+// // 	url: "http://localhost/~ruanpingcheng/product3.json",
+// 	url: "http://localhost/~ruanpingcheng/FYPFrontEnd/test/emotion.json",
+//
+// 	type: "GET",
+// 	dataType:"json",
+// 	error:function(XMLHttpRequest, textStatus, errorThrown) {
+// 		alert(errorThrown);
+// 		        }    ,
+// 	success:function(data){
+// 		console.log(data.sentiment);
+// 		console.log(data.emotions);
+//
+// 	}
+// 	});
+
 	
 	//console.log("fineChinese");
 	 
 }
 
 function coarseEnglish(s){  
-	console.log("coarseEnglish");
+	$.getJSON("test/negative.json",s,sentiCallback);
 	
 }
 
 function fineEnglish(s){  
-	console.log("fineEnglish");
+	$.getJSON("test/emotion.json",s,emotionCallback);
 	
 }
 

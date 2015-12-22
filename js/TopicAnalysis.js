7866c595c1b5ddcd026e97676eed81fff4bd5479
Ruan Pingcheng <ruanpingcheng@gmail.com>
2015-12-22 10:15:26 +0800

    Finish the bar chart

--- js/TopicAnalysis.js
+++ js/TopicAnalysis.js
@@ -29,22 +29,25 @@ $("#paragraph").click(function(){
 	var animateBar=setInterval(animateProgressBar,1000)
 	
 	$("#waiting").show();
-	// if(containChinese(posContents) || containChinese(negContents)){
-//
-// 		chinese(posContents,negContents);
-// 	}else{
-// 		english(posContents,negContents);
-// 	}
+	var content = $("content").val();
+	var arguments = constructArgument();
+	if(containChinese(content)){
+
+		chinese(arguments,content);
+	}else{
+		english(arguments,content);
+	}
 
 
 });
 
 function hasEmptyInput(){
 	var r = false;
-	$("input").each(function(){
+	$("input ").each(function(){
 		var t = $(this).val();
 		if (t.replace(/(^s*)|(s*$)/g, "").length ==0){ 
 			r = true;
+			console.log($(this));
 			
 			alert('Some text input is empty!'); 	
 		}
@@ -167,13 +170,13 @@ function prepareAliasHTML(topicId,aliasId){
 
 }
 
-function chinese(pos,neg){  
+function english(arguments,content){  
 	//Change later
-	//$.getJSON("test/polarity.json",null,callback);
+	$.getJSON("test/topic.json",null,callback);
 
 	//  $.ajax({
 // // 	url: "http://localhost/~ruanpingcheng/product3.json",
-// 	url: "http://localhost/~ruanpingcheng/FYPFrontEnd/test/polarity.json",
+// 	url: "http://localhost/~ruanpingcheng/FYPFrontEnd/test/topic.json",
 //
 // 	type: "GET",
 // 	dataType:"json",
@@ -181,15 +184,15 @@ function chinese(pos,neg){
 // 		alert(errorThrown);
 // 		        }    ,
 // 	success:function(data){
-// 		console.log(data.positive);
-// 		console.log(data.negative);
+// 		console.log(data);
+// 		//console.log(data.negative);
 //
 // 	}
 // 	});
 }
 
-function english(pos,neg){  
-	//$.getJSON("test/polarity.json",null,callback);
+function chinese(arguments,content){  
+	$.getJSON("test/topic.json",null,callback);
 }
 
 
@@ -204,7 +207,329 @@ function animateProgressBar(){
 
 
 function callback(data){
+	$("#waiting").hide();
+	var topics = [];
+	var posVals = [];
+	var negVals = [];
+	var neuVals = [];
+	var mixVals = [];
 	
+	for(var key in data){
+		var obj = data[key];
+		topics.push(obj.topic);
+		posVals.push(obj.positive);
+		negVals.push(obj.negative);
+		neuVals.push(obj.neutral);
+		mixVals.push(obj.mixed);
+	}
+	setTheme();
+	$("#chart").highcharts({
+		chart: {
+		            type: 'column'
+		        },
+		        title: {
+		            text: 'Sentiment Distribution for Each Topic'
+		        },
+		        subtitle: {
+		            text: 'From SentiAgentPlus Backend'
+		        },
+		        xAxis: {
+				categories: topics,
+    		            title: {
+    		                text: 'Topics'
+    		            }
+		        },
+		        yAxis: {
+		            min: 0,
+		            title: {
+		                text: 'Sentence Number'
+		            }
+		        },
+		        tooltip: {
+         		    headerFormat: '<span style="font-size:10px">Topic: {point.key}</span><table>',		
+		            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+		                '<td style="padding:0"><b>{point.y:.f}</b></td></tr>',
+		            footerFormat: '</table>',
+		            shared: true,
+		            useHTML: true
+		        },
+		        plotOptions: {
+		            column: {
+		                pointPadding: 0.2,
+		                borderWidth: 0
+		            }
+		        },
+		        series: [{
+		            name: 'Positive',
+				data: posVals
+
+		       	 	},
+				{
+			    name: 'Negative',
+			    data: negVals
+				},
+				{
+			    name: 'Neutral',
+			    data: neuVals
+				},
+				{
+			    name: 'Mixed',
+			    data: mixVals
+				}
+			]
+	});
 	
 }
 
+
+function setTheme(){
+	Highcharts.theme = {
+		colors: ["#DDDF0D", "#55BF3B", "#DF5353", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
+			"#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
+		chart: {
+			backgroundColor: {
+				linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+				stops: [
+					[0, 'rgb(48, 48, 96)'],
+					[1, 'rgb(0, 0, 0)']
+				]
+			},
+			borderColor: '#000000',
+			borderWidth: 2,
+			className: 'dark-container',
+			plotBackgroundColor: 'rgba(255, 255, 255, .1)',
+			plotBorderColor: '#CCCCCC',
+			plotBorderWidth: 1
+		},
+		title: {
+			style: {
+				color: '#C0C0C0',
+				font: 'bold 26px "Trebuchet MS", Verdana, sans-serif'
+			}
+		},
+		subtitle: {
+			style: {
+				color: '#666666',
+				font: 'bold 20px "Trebuchet MS", Verdana, sans-serif'
+			}
+		},
+		xAxis: {
+			gridLineColor: '#333333',
+			gridLineWidth: 1,
+			labels: {
+				style: {
+					color: '#A0A0A0'
+				}
+			},
+			lineColor: '#A0A0A0',
+			tickColor: '#A0A0A0',
+			title: {
+				style: {
+					color: '#CCC',
+					fontWeight: 'bold',
+					fontSize: '18px',
+					fontFamily: 'Trebuchet MS, Verdana, sans-serif'
+
+				}
+			}
+		},
+		yAxis: {
+			gridLineColor: '#333333',
+			labels: {
+				style: {
+					color: '#A0A0A0'
+				}
+			},
+			lineColor: '#A0A0A0',
+			minorTickInterval: null,
+			tickColor: '#A0A0A0',
+			tickWidth: 1,
+			title: {
+				style: {
+					color: '#CCC',
+					fontWeight: 'bold',
+					fontSize: '18px',
+					fontFamily: 'Trebuchet MS, Verdana, sans-serif'
+				}
+			}
+		},
+		tooltip: {
+			backgroundColor: 'rgba(0, 0, 0, 0.75)',
+			style: {
+				color: '#F0F0F0'
+			}
+		},
+		toolbar: {
+			itemStyle: {
+				color: 'silver'
+			}
+		},
+		plotOptions: {
+			line: {
+				dataLabels: {
+					color: '#CCC'
+				},
+				marker: {
+					lineColor: '#333'
+				}
+			},
+			spline: {
+				marker: {
+					lineColor: '#333'
+				}
+			},
+			scatter: {
+				marker: {
+					lineColor: '#333'
+				}
+			},
+			candlestick: {
+				lineColor: 'white'
+			}
+		},
+		legend: {
+			itemStyle: {
+				font: '12pt Trebuchet MS, Verdana, sans-serif',
+				color: '#A0A0A0'
+			},
+			itemHoverStyle: {
+				color: '#FFF'
+			},
+			itemHiddenStyle: {
+				color: '#444'
+			}
+		},
+		credits: {
+			style: {
+				color: '#666'
+			}
+		},
+		labels: {
+			style: {
+				color: '#CCC'
+			}
+		},
+
+		navigation: {
+			buttonOptions: {
+				symbolStroke: '#DDDDDD',
+				hoverSymbolStroke: '#FFFFFF',
+				theme: {
+					fill: {
+						linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+						stops: [
+							[0.4, '#606060'],
+							[0.6, '#333333']
+						]
+					},
+					stroke: '#000000'
+				}
+			}
+		},
+
+		// scroll charts
+		rangeSelector: {
+			buttonTheme: {
+				fill: {
+					linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+					stops: [
+						[0.4, '#888'],
+						[0.6, '#555']
+					]
+				},
+				stroke: '#000000',
+				style: {
+					color: '#CCC',
+					fontWeight: 'bold'
+				},
+				states: {
+					hover: {
+						fill: {
+							linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+							stops: [
+								[0.4, '#BBB'],
+								[0.6, '#888']
+							]
+						},
+						stroke: '#000000',
+						style: {
+							color: 'white'
+						}
+					},
+					select: {
+						fill: {
+							linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+							stops: [
+								[0.1, '#000'],
+								[0.3, '#333']
+							]
+						},
+						stroke: '#000000',
+						style: {
+							color: 'yellow'
+						}
+					}
+				}
+			},
+			inputStyle: {
+				backgroundColor: '#333',
+				color: 'silver'
+			},
+			labelStyle: {
+				color: 'silver'
+			}
+		},
+
+		navigator: {
+			handles: {
+				backgroundColor: '#666',
+				borderColor: '#AAA'
+			},
+			outlineColor: '#CCC',
+			maskFill: 'rgba(16, 16, 16, 0.5)',
+			series: {
+				color: '#7798BF',
+				lineColor: '#A6C7ED'
+			}
+		},
+
+		scrollbar: {
+			barBackgroundColor: {
+					linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+					stops: [
+						[0.4, '#888'],
+						[0.6, '#555']
+					]
+				},
+			barBorderColor: '#CCC',
+			buttonArrowColor: '#CCC',
+			buttonBackgroundColor: {
+					linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+					stops: [
+						[0.4, '#888'],
+						[0.6, '#555']
+					]
+				},
+			buttonBorderColor: '#CCC',
+			rifleColor: '#FFF',
+			trackBackgroundColor: {
+				linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+				stops: [
+					[0, '#000'],
+					[1, '#333']
+				]
+			},
+			trackBorderColor: '#666'
+		},
+
+		// special colors for some of the
+		legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
+		background2: 'rgb(35, 35, 70)',
+		dataLabelsColor: '#444',
+		textColor: '#C0C0C0',
+		maskColor: 'rgba(255,255,255,0.3)'
+	};
+
+	// Apply the theme
+	var highchartsOptions = Highcharts.setOptions(Highcharts.theme);
+}
